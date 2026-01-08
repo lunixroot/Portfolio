658d93cbbc1baa29459f7c80533876f2328a2b14
Adarsh <adarsht740@gmail.com>
2026-01-08 16:00:36 +0530

    newaddons

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -3,10 +3,10 @@ const nodemailer = require('nodemailer')
 const router = express.Router()
 
 router.post('/send', async (req, res) => {
-    try{
-        const {name, email, message} = req.body;
+    try {
+        const { name, email, message } = req.body;
 
-        if(!name || !email || !message) {
+        if (!name || !email || !message) {
             return res.status(400).json({
                 success: false,
                 message: 'Name, Your Email and Message is needed! 🤧'
@@ -20,27 +20,47 @@ router.post('/send', async (req, res) => {
                 pass: process.env.EMAIL_PASSWORD
             }
         })
-        const mailOptions = {
-            from: email,
+
+
+        // const mailOptions = {
+        //     from: email,
+        //     to: process.env.EMAIL_USER,
+        //     subject: `Portfolio Contact from ${name}`,
+        //     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+        //     html: `<h3>New Contact Form Submission</h3>
+        //            <p><strong>Name:</strong> ${name}</p>
+        //            <p><strong>Email:</strong> ${email}</p>
+        //            <p><strong>Message:</strong> ${message}</p>`
+        // }
+
+        const ownerMailOptions = {
+            from: process.env.EMAIL_USER,
             to: process.env.EMAIL_USER,
-            subject: `Portfolio Contact from ${name}`,
-            text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-            html: `<h3>New Contact Form Submission</h3>
-                   <p><strong>Name:</strong> ${name}</p>
-                   <p><strong>Email:</strong> ${email}</p>
-                   <p><strong>Message:</strong> ${message}</p>`
+            subject: `new contact from ${name}`,
+            text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`
         }
 
-        await transporter.sendMail(mailOptions)
+        const userMailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'thank you for contacting me!',
+            text: `Hi ${name},\n\nThank you for your message!\n\nBest regards,\nAdarsh`
+        }
+
+        // await transporter.sendMail(mailOptions)
+
+         await transporter.sendMail(ownerMailOptions)
+        await transporter.sendMail(userMailOptions)
+
 
         res.status(200).json({
             success: true,
             message: 'email successfully sent'
         })
-    } catch(error){
+    } catch (error) {
         console.error('error:', error)
         res.status(500).json({
-            success:false,
+            success: false,
             message: 'email error',
             error: error.message
         })
